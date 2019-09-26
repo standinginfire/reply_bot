a9ad0d58b261316dcbada3e511fe208584504e9e
Dmytro Butrynskyy <dmytro.butrynskyy@edvantis.com>
2019-09-26 20:38:36 +0300

    added functionality to send messages to chat id

--- bot.js
+++ bot.js
@@ -27,48 +27,70 @@ let extractData = (message_text) => {
   return {};
 }
 
-let previous_messages = {};
-
 bot.on('message', async (context, next) => {
   console.log('-----> message');
+  console.log('----->session:');
+  console.log(context.session);
   if(context.updateSubTypes.includes('forward')) {
-    previous_messages[context.from.id] = context.message;
-    console.log(`added ${Object.keys(previous_messages)} ${previous_messages[context.from.id]}`)
+    context.session.forward = context.message;
+    console.log(`added ${context.session.forward}`)
   }
-  else if(Object.keys(previous_messages).includes(context.from.id)) {
-    let source_message = previous_messages[context.from.id];
-    telegram.sendMessage(source_message.forward_from_chat_id, context.message.text, { reply_to_message_id : source_message.forward_from_chat_message_id});
-    delete previous_messages(context.from.id);
+  else if(context.session.forward) {
+    tg.sendMessage(context.session.forward.forward_from_chat_id, context.message.text, { reply_to_message_id : context.session.forward.forward_from_chat_message_id});
+    delete previous_messages(context.session.forward);
+    return;
+  }
+  else if(context.session.chat_id) {
+    console.log(`sending message ${context.message.text} to ${context.session.chat_id}`);
+    console.log(await tg.sendMessage(context.session.chat_id, context.message.text));
+    return;
   }
   return next(context);
 })
 
 bot.command('message', async (context, next) => {
   console.log(context);
-  let data = extractData(context.message.text);
-  tg.sendMessage(data.chat_id, data.text)
+  try {
+    let data = extractData(context.message.text);
+    context.session.chat_id = data.chat_id || context.session.chat_id;
+    if(context.session.chat_id) {
+      context.reply(`sending message to ${context.session.chat_id}`);
+      tg.sendMessage(data.chat_id, data.text)
+    }
+    else {
+      context.reply(`chat id was not set; chat id passed: ${data.chat_id}`);
+    }
+  }
+  catch(err) {
+    context.reply(err);
+  }
+  next(context);
 })
 
 bot.command('set_chat', async(context, next) => {
   console.log('-----> set chat')
   let id;
   try {
-    
-    let res = /set_chat (?<id>-?[0-9]*)/.exec(context.message.text);
+    let res = /set_chat (?<id>-?[0-9]*)/.exec(context.message.text); //mb change to extractData call
     id = res.groups.id;
   } 
   catch(e){
     context.reply(e);
   }
   if(id) {
-    context.id = id;
-    let chat = await tg.getChat(id);
+    context.session.chat_id = id;
+    let chat = await tg.getChat(context.session.chat_id);
     console.log(chat);
   }
 })
+
+bot.command('reset', async(context, next) => {
+  context.session = {};
+})
+
 bot.command('id', async (context, next) => {
   console.log('-----> id');
-  tg.sendMessage(context.chat.id, context.chat.id);
+  context.reply(context.chat.id);
 })
 
 bot.startPolling();
